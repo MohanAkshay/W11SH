2e631320266e0b69660ff9150a6c5f97885c2858
MohanAkshay <33710243+MohanAkshay@users.noreply.github.com>
2018-10-13 23:37:31 +0530

    Add files via upload

---
+++ TestMainline.java
@@ -0,0 +1,11 @@
+package nQueens;
+
+public class TestMainline {
+
+	public static void main(String[] args) {
+		GameBoard Test5By5 = new GameBoard(5);
+		Test5By5.place(2, 2);
+		System.out.println("Mohan Akshay Bhogadi"+ "\n" + Test5By5);
+	}
+
+}
